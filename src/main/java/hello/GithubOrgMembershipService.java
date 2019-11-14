78fe30a6be8f455d78029b8bdd0293dcddc9850f
Tahereh Mehrjerdy <tahereh@umail.ucsb.edu>
2019-11-13 20:05:22 -0800

    resolved javadoc problem

--- src/main/java/hello/GithubOrgMembershipService.java
+++ src/main/java/hello/GithubOrgMembershipService.java
@@ -37,14 +37,20 @@ public class GithubOrgMembershipService implements MembershipService {
         logger.info("githubOrg=" + githubOrg);
     }
 
-    /**
-     * is current logged in user a member but NOT an admin of the github org
-     */
+    /** check membership
+
+     * @param oAuth2AuthenticationToken oauth token 
+     * @return  is current logged in user a member but NOT an admin of the github org?
+     **/
     public boolean isMember(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
         return hasRole(oAuth2AuthenticationToken, "member");
     }
 
-    /** is current logged in user a member of the github org */
+    /** check membership
+ * @param oAuth2AuthenticationToken oauth token
+ * @return  is current logged in user a member of the github org ?
+
+ **/
     public boolean isAdmin(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
         return hasRole(oAuth2AuthenticationToken, "admin");
     }
